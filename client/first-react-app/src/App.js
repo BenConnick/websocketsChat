a05df5f1467b98abfdab6ddf9c4b4e4aaea29755
Ben <bxc3201@rit.edu>
2018-09-01 20:44:11 -0500

    auto commit

--- client/first-react-app/src/App.js
+++ client/first-react-app/src/App.js
@@ -508,6 +508,7 @@ class App extends Component {
           ref={(c) => { chat = c; }}
           className={this.state.chatClass}
         />
+        <div id="fade"></div>
       </div>
     );
   }
